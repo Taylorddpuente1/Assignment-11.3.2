f0aedfee99be5d9de9b3d2d125b0d4d56e018877
Taylorddpuente1 <taylor.puente@outlook.com>
2025-06-24 16:12:19 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿//return the majority elemnt 
+using System;
+
+
+public class Program
+{
+    public static void Main()
+    {
+        int[] nums = { 2, 2, 1, 1, 1, 2, 2 };
+        int result = MajorityElement(nums);
+
+        Console.WriteLine(result);
+    }
+
+    public static int MajorityElement(int[] nums)
+    {
+        int count = 0;
+        int candidate = 0;
+
+        foreach (int num in nums)
+        {
+            if (count == 0)
+            {
+                candidate = num;
+            }
+
+            if (num == candidate)
+            {
+                count++;
+            }
+            else
+            {
+                count--;
+            }
+        }
+
+        return candidate;
+    }
+}
